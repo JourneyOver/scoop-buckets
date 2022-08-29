dea26dc8a68355cb4a82c04d52f51e88d848a4db
Journey <timtag1190@gmail.com>
2022-08-29 04:46:29 -0500

    Initial commit

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "JourneyOver/scoop-buckets:master"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
