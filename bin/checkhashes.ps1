aaed274c50aef0820fce519b1dd1f4f9bfda5837
Journey <timtag1190@gmail.com>
2022-11-23 05:36:34 -0600

    Update CI and templates

--- bin/checkhashes.ps1
+++ bin/checkhashes.ps1
@@ -1,4 +1,4 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
 $checkhashes = "$env:SCOOP_HOME/bin/checkhashes.ps1"
-$dir = "$psscriptroot/../bucket" # checks the parent dir
-Invoke-Expression -command "& '$checkhashes' -dir '$dir' $($args | ForEach-Object { "$_ " })"
+$dir = "$PSScriptRoot/../bucket" # checks the parent dir
+& $checkhashes -Dir $dir @Args
